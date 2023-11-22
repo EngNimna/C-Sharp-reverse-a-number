5e230afa68ea069b57a2f7958b7663deddc52620
nimna nilwala <nimna.nilwala555@gmail.com>
2023-11-22 20:43:23 +0530

    Add project files.

---
+++ cSharpReverseOfNumber0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cSharpReverseOfNumber0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num2, rem = 0, rev = 0;
+            Console.Write("Enter the number...: ");
+            num = int.Parse(Console.ReadLine());
+            num2 = num;
+
+            while (num > 0)
+            {
+                rem = num % 10;
+                num = num / 10;
+                rev = rev * 10 + rem;
+            }
+            Console.WriteLine("The number you entered = " + num2 + "                       The reverse of that number is = " + rev);
+        }
+    }
+}
